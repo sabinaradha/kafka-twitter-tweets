96d3cf3e1f90bc41fb556b25ce5a6b04608639d9
Sabina Koirala <sbnkrl@gmail.com>
2020-11-19 09:40:02 -0600

    Added producer safe code and reformatted the code

--- kafka-producer-twitter/src/main/java/kafka/tutorial2/TwitterProducer.java
+++ kafka-producer-twitter/src/main/java/kafka/tutorial2/TwitterProducer.java
@@ -1,4 +1,4 @@
-package com.github.simplekafka.kafka.tutorial2;
+package kafka.tutorial2;
 
 import com.google.common.collect.Lists;
 import com.twitter.hbc.ClientBuilder;
@@ -29,7 +29,7 @@ public class TwitterProducer {
     String token="";
     String secret="";
 
-    List<String> terms = Lists.newArrayList("kafka");
+    List<String> terms = Lists.newArrayList("bitcoin", "usa", "soccer", "politics", "sport");
 
     public TwitterProducer(){}
 
@@ -116,6 +116,17 @@ public class TwitterProducer {
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //create safe producer
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+
+        //high throughput producer at the expense of a bit of latency and CPU usage
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32*1024));
 
         //create producer
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
